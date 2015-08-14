adeb675398403dec6faee353e301b757a43acbe6
kimu_shu <alfvegardrisc@gmail.com>
2015-08-14 12:43:03 +0900

    Change generate_ck arguments

--- generate_ck.sh
+++ generate_ck.sh
@@ -3,19 +3,19 @@ input=$1
 output=$2
 shift 2
 if [[ -z "$input" || -z "$output" ]]; then
-  echo "usage: $0 <input> <output> [<options>]"
+  echo "usage: $0 <input> <output> [<options_for_ckenc>]"
   exit 1
 fi
 tmp="tmp_${output##*/}"
 mkdir -p $tmp
-cmd="ffmpeg -i $input $* -vf scale=320:240,transpose=1 $tmp/video_%05d.bmp"
+cmd="ffmpeg -i $input -r 10 -vf scale=320:240,transpose=1 $tmp/video_%05d.bmp"
 echo $cmd
 $cmd
 wav="$tmp/audio.wav"
-cmd="ffmpeg -i $input -vn -ar 15625 -ac 1 -acodec pcm_u8 $wav"
+cmd="ffmpeg -i $input -y -vn -ar 15625 -ac 1 -acodec pcm_u8 $wav"
 echo $cmd
 $cmd
-cmd="./ck_enc -a $wav $tmp/video"
+cmd="./ck_enc -a $wav $* $tmp/video"
 echo $cmd
 $cmd
 mv $tmp/video.ck $output
